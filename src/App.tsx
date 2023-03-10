7eaaaaa8dc406cc9938d18552dcd7b0b685d12f3
Alvin Lam <80093619+alvinlamSL@users.noreply.github.com>
2023-03-10 16:56:41 +0800

    Create suggestedKey state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '@mui/material';
 
 import './App.css';
@@ -6,13 +6,20 @@ import Banner from './components/Banner';
 import TextDisplay from './components/TextDisplay/index';
 import KeyboardDisplay from './components/KeyboardDisplay';
 
+export interface SuggestedKey {
+    key: string
+    isCaps: boolean
+}
+
 const App: React.FC = () => {
+    const [suggestedKey, setSuggestedKey] = useState<SuggestedKey>({ key: '', isCaps: false });
+
     return (
         <div className="App">
             <Container maxWidth="md">
                 <Banner/>
-                <TextDisplay/>
-                <KeyboardDisplay/>
+                <TextDisplay setSuggestedKey={setSuggestedKey} />
+                <KeyboardDisplay suggestedKey={suggestedKey} />
             </Container>
         </div>
     );
