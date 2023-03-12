b175d6732487ccac18e1e41fd24ff828fd88a933
Alvin Lam <80093619+alvinlamSL@users.noreply.github.com>
2023-03-12 20:26:21 +0700

    Updates

--- src/components/KeyboardDisplay/KeyboardDisplay.styles.ts
+++
@@ -1,9 +0,0 @@
-import { type SxProps } from '@mui/system';
-
-const styles: Record<string, SxProps> = {
-    main: {
-        height: 'calc(60vh - 60px)',
-    },
-};
-
-export default styles;
--- src/components/KeyboardDisplay/KeyboardLayout/KeyboardLayout.styles.ts
+++
@@ -1,9 +0,0 @@
-import { type SxProps } from '@mui/system';
-
-const styles: Record<string, SxProps> = {
-    main: {
-        padding: 1,
-    },
-};
-
-export default styles;
