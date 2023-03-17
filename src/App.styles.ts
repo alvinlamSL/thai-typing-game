3ed77ebe454240a6d94df55f9dc4279fd328f466
Alvin Lam <80093619+alvinlamSL@users.noreply.github.com>
2023-03-17 23:02:56 +0700

    Change padding size for mobile

---
+++ src/App.styles.ts
@@ -0,0 +1,16 @@
+import { type SxProps } from '@mui/system';
+
+const styles: Record<string, SxProps> = {
+    appContainer: {
+        paddingLeft: {
+            xs: 0,
+            md: 2,
+        },
+        paddingRight: {
+            xs: 0,
+            md: 2,
+        },
+    },
+};
+
+export default styles;
