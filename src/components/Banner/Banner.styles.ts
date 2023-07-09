c845e2c9edbc08c4fa166a8678210ae1a730c5e6
Alvin Lam <80093619+alvinlamSL@users.noreply.github.com>
2023-07-10 00:25:18 +0800

    Add title to banner

--- src/components/Banner/Banner.styles.ts
+++ src/components/Banner/Banner.styles.ts
@@ -3,8 +3,27 @@ import { type SxProps } from '@mui/system';
 const styles: Record<string, SxProps> = {
     main: {
         height: '120px',
-        bgcolor: 'green',
     },
+    toolbar: {
+        height: '120px',
+        backgroundColor: 'cornflowerblue',
+    },
+    title: {
+        alignSelf: 'flex-end',
+        padding: 1,
+        fontSize: {
+            xs: '1.25rem',
+            md: '1.5rem',
+        },
+        lineHeight: {
+            xs: '1.6',
+            md: '1.334',
+        },
+        letterSpacing: {
+            xs: '0.0075em',
+            md: '0em'
+        },
+    }
 };
 
 export default styles;
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import { Box } from '@mui/system';
+import {
+    AppBar,
+    Toolbar,
+    Typography
+} from '@mui/material';
+
+import ReducerContext from '../../reducer/reducerContext';
 
 import styles from './Banner.styles';
 
 const Banner: React.FC = () => {
+    const { state } = useContext(ReducerContext);
+    const { lessonTitle } = state;
+
     return (
         <Box
             sx={{
                 ...styles?.main,
             }}
         >
-            Banner
+            <AppBar position="static">
+                <Toolbar
+                    sx={{ ...styles.toolbar }}
+                >
+                    <Typography
+                        sx={{ ...styles.title }}
+                        fontWeight="bold"
+                        noWrap
+                        component="div"
+                    >
+                        {lessonTitle}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </Box>
     );
 };
