9f8581d6bdcd25128bcfadb84ac3188c4bbb9276
Alvin Lam <80093619+alvinlamSL@users.noreply.github.com>
2023-07-09 20:26:42 +0800

    Grab lesson from public folder on init

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,34 @@ import { Container } from '@mui/material';
 
 import ReducerContext from './reducer/reducerContext';
 import { reducer } from './reducer';
+import { setLesson } from './reducer/actions';
 import { initialState } from './reducer/state';
 
 import Banner from './components/Banner';
 import TextDisplay from './components/TextDisplay/index';
 import KeyboardDisplay from './components/KeyboardDisplay';
 
+import { type Lesson } from './types';
+
 import styles from './App.styles';
 
 const App: React.FC = () => {
     const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    React.useEffect(() => {
+        fetch(`${process.env.PUBLIC_URL}/lessons/lesson1.json`)
+            .then(async (f) => await f.json())
+            .then((data) => {
+                const lesson: Lesson = {
+                    title: data.title,
+                    engScripts: data.engScripts,
+                    engPhonemeScripts: data.engPhonemeScripts,
+                    thaiPhonemeScripts: data.thaiPhonemeScripts,
+                };
+
+                dispatch(setLesson(lesson));
+            }, () => {});
+    }, []);
 
     return (
         <ReducerContext.Provider value={{ state, dispatch }}>
